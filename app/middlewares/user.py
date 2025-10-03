c8822f5d781a069c07a55cbb25bb460d9f1f37fe
devvsima <toppochka@gmail.com>
2025-10-03 21:11:58 +0300

    Fix bug

--- app/middlewares/user.py
+++ app/middlewares/user.py
@@ -13,8 +13,8 @@ class UsersMiddleware(BaseMiddleware):
     ) -> Any:
         session = data["session"]
         user, is_create = await User.get_or_create(
-            session,
-            user_id=message.from_user.id,
+            session=session,
+            id=message.from_user.id,
             username=message.from_user.username,
             language=message.from_user.language_code,
         )
